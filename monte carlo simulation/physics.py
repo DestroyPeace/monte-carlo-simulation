2b2f501c582b4d815c537ee3e188236704ddc0d5
DestroyPeace <Destroypeaces@gmail.com>
2022-03-04 21:25:59 +0000

    Changed to animating the actual spheres rather than calculate all of the values however need to fix the physics logic behind it. Keeping the class in future for detecting the direction of the normal collisions, and then need to create the blender files to simulate this.

--- monte carlo simulation/physics.py
+++ monte carlo simulation/physics.py
@@ -36,7 +36,9 @@ class Sphere(Entity):
         return self.velocity
 
     
-    def get_bounce(self):
+    def get_bounce_time(self):
+        self.bouncing = True
+        self.velocity = self.get_velocity()
         # https://www.physicsforums.com/threads/bouncing-ball-equation.403229/
         # Essentially, assume that all the energy has been fully converted to a kinetic store from a 
         # gravitational potential store (negative because we're going in opposite direction) and multiply
@@ -47,30 +49,22 @@ class Sphere(Entity):
         # Because of the fact that the rebound velocity is going to change, assuming no drag therefore it'll change instanteously,
         # the kinetic energy is going to change and the change in energy is simply calculated by recalculating the kinetic energy
         # and then applying that onto the new amx_height which'll be using the new rebound_velocity as well as the potential energy.
+        if self.velocity != 0:
+            rebound_velocity = self.velocity * self.constant_of_restituion * -1
+            potential_energy = 1/2 * self.mass * rebound_velocity ** 2
 
-        rebound_velocity = self.velocity * self.constant_of_restituion * -1
-        potential_energy = 1/2 * self.mass * rebound_velocity ** 2
+            max_height = rebound_velocity ** 2 / (2 * 9.8 * sqrt(2*potential_energy))
 
-        max_height = rebound_velocity ** 2 / (2 * 9.8 * sqrt(2*potential_energy))
-        return rebound_velocity, max_height
+            print(max_height / rebound_velocity)
+            return max_height / rebound_velocity
+        
+        return 0
 
 def update():
     # Iterating and calculating the velocity of every individual sphere by assuming gravitational constant of 9.8m/s**2
     # Checking whether or not intersection has occured with the plane (perhaps use raycasting) and then calculating bouncing.
-    for index, sphere in enumerate(spheres):
-        # Getting rid of any spheres that have passed the cube (temporary solution, replace with hopefully raycasting if it isn't too slow)
-        if sphere.position[1] < (ground.Y + ground.world_scale_y / 2 - 20) and not(sphere.bouncing):
-            destroy(sphere)
-            spheres.pop(index)
-
-        elif not(sphere.intersects(ground).hit) and not(sphere.bouncing):
-            sphere.position -= (0, sphere.get_velocity(), 0)
-
-        else:
-            velocity, max_height = sphere.get_bounce()
-            
-            if sphere.bouncing and sphere.world_position[1] <= max_height:
-                sphere.world_position[1] -= velocity
+    for sphere in spheres:
+        sphere.animate_y(-200, duration = sphere.get_bounce_time(), curve = curve.out_bounce)
 
 
 
@@ -90,8 +84,6 @@ def input(key):
 app = Ursina()
 
 spheres = []
-sphere = Sphere(model = "sphere", scale = (2, 2, 2), position = (0, 200, 0), collider = "sphere")
-spheres.append(sphere)
 
 ground = Entity(model = "cube", scale = (200, 50, 200), position = (0, -200, 0), color = color.black,  collider = "box")
 
