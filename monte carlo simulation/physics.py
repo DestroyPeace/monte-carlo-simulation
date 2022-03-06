39fe0bafa3fb8d3c8574a4e84cc027869e02bee3
DestroyPeace <Destroypeaces@gmail.com>
2022-03-06 18:14:38 +0000

    Added the animation for calculating the distance, and rather than distance(), I used the get_position with relative_to ground and then accounted for the scaling of each individual entity.

--- monte carlo simulation/physics.py
+++ monte carlo simulation/physics.py
@@ -6,15 +6,15 @@ class Sphere(Entity):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.time = 0
-        self.velocity = 0
+        self.constant_of_restituion = 0.7
         self.air_mass_density = 1.125
-        self.constant_of_restituion = 0.5
         self.bouncing = False
         
+        self.max_height = abs((self.world_position[1] + self.world_scale_y / 2) - abs(ground.world_scale_y / 2 + ground.world_position[1]))
         self.mass = self.world_scale_z ** 3 * 4/3 * pi 
 
 
-    def get_velocity(self):
+    def get_velocity(self) -> int:
         #   f = ma, therefore a = m/f where f = force, m = mass, a = acceleration
         #   f(air) = -cv**2 where c = force constant, v = velocity of object and - means opposite direction.
         #   c = f(air) / v ** 2 with no negative because we're not calculating an opposite force direction
@@ -36,9 +36,8 @@ class Sphere(Entity):
         return self.velocity
 
     
-    def get_bounce_time(self):
+    def get_bounce_time(self) -> int:
         self.bouncing = True
-        self.velocity = self.get_velocity()
         # https://www.physicsforums.com/threads/bouncing-ball-equation.403229/
         # Essentially, assume that all the energy has been fully converted to a kinetic store from a 
         # gravitational potential store (negative because we're going in opposite direction) and multiply
@@ -49,23 +48,16 @@ class Sphere(Entity):
         # Because of the fact that the rebound velocity is going to change, assuming no drag therefore it'll change instanteously,
         # the kinetic energy is going to change and the change in energy is simply calculated by recalculating the kinetic energy
         # and then applying that onto the new amx_height which'll be using the new rebound_velocity as well as the potential energy.
+        
+        self.velocity = sqrt(2 * 9.8 * self.max_height) # CALCULATED BY SETTING KINETIC ENERGY EQUAL TO GPE AND SOLVING FOR V
+
         if self.velocity != 0:
             rebound_velocity = self.velocity * self.constant_of_restituion * -1
             potential_energy = 1/2 * self.mass * rebound_velocity ** 2
 
-            max_height = rebound_velocity ** 2 / (2 * 9.8 * sqrt(2*potential_energy))
-
-            print(max_height / rebound_velocity)
-            return max_height / rebound_velocity
+            return abs(self.max_height / rebound_velocity) # RETURNING ABSOLUTE BECAUSE ATTEMPTING TO FIND TIME RATHER THAN A RESULTANT FORCE.
         
         return 0
-
-def update():
-    # Iterating and calculating the velocity of every individual sphere by assuming gravitational constant of 9.8m/s**2
-    # Checking whether or not intersection has occured with the plane (perhaps use raycasting) and then calculating bouncing.
-    for sphere in spheres:
-        sphere.animate_y(-200, duration = sphere.get_bounce_time(), curve = curve.out_bounce)
-
 
 
 def input(key):
@@ -74,18 +66,20 @@ def input(key):
             destroy(sphere)
 
         spheres.clear()
-    
-    # Counting number of spheres
+
     elif key == "c":
-        new_sphere = Sphere(model = "sphere", scale = (10, 10, 10), color = color.orange, position = (randint(-100, 100), randint(0, 500), randint(-100, 100)), collider = "sphere")
+        new_sphere = Sphere(model = "sphere", scale = (10, 10, 10), color = color.orange, position = (randint(-100, 100), randint(200, 700), randint(-100, 100)), collider = "sphere")
         spheres.append(new_sphere)
 
+        # Animating with a distance of the relative position subtract the scale (because the scale explodes from the origin of (0, 0, 0) and therefore must be
+        # accounted for.).
+        new_sphere.animate_y(-1 * (new_sphere.get_position(relative_to = ground)[1] - ground.world_scale_y) - new_sphere.world_scale_y, duration = new_sphere.get_bounce_time(), curve = curve.out_bounce)
 
 app = Ursina()
 
 spheres = []
 
-ground = Entity(model = "cube", scale = (200, 50, 200), position = (0, -200, 0), color = color.black,  collider = "box")
+ground = Entity(model = "cube", scale = (200, 50, 200), position = (0, 0, 0), color = color.white,  collider = "box")
 
 EditorCamera()
 app.run()
